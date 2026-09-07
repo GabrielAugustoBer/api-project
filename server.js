fab13ec5efda6e43abfbd3105d3d31043291190b
gabrielabl <gabriel.linobernardes@gmail.com>
2026-09-07 11:22:24 -0300

    feature: routes and database memory with tests routes

---
+++ server.js
@@ -0,0 +1,54 @@
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify()
+
+const database = new DatabaseMemory()
+
+server.post('/videos', (request, reply) => {
+    const {title, description, duration} = request.body
+
+    database.create({   
+        title: title,
+        description: description,
+        duration: duration
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', (request ) => {
+    const search = request.query.search
+    const videos = database.list(search)
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const {title, description, duration} = request.body
+
+    database.update(videoId, {
+        title: title,
+        description: description,
+        duration: duration
+    })
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    
+    database.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
+
+//GET: Busca alguma info(listagem, detalhes)
+//POST: Criação de registro
+//PUT: Alteração
+//DELETE: Deleta
